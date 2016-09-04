73b014c2dc0a0ce28a98b05d2422624c15adce31
Akasha Archer <akashaarcher@Akashas-MacBook-Air.local>
2016-09-04 10:41:42 -0400

    Add morning challenge

---
+++ src/nyc/c4q/akashaarcher/Student.java
@@ -0,0 +1,62 @@
+package nyc.c4q.akashaarcher;
+
+/**
+ * Created by akashaarcher on 9/4/16.
+ */
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String faveFood;
+
+}
+
+    public Student() {
+
+    }
+
+    private static boolean checkSameFavoriteFood() {
+        if (Student.faveFood() instanceof Student.faveFood()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+
+    private static void getFirstName() {
+
+    }
+
+
+    private static void setFirstName() {
+
+    }
+
+    private static void getLastName() {
+
+    }
+
+    private static void setLastName() {
+
+    }
+
+    private static void getIdNumber() {
+
+    }
+
+    private static void setIdNumber() {
+
+    }
+
+    private static void getFaveFood() {
+
+    }
+
+    private static void setFaveFood() {
+
+    }
